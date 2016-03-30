dbfff8a48c05fd1172999e2744a5b240c0815d65
jenb <jjbordafunsanmateo.edu.co>
2016-03-30 13:36:45 -0500

    creando los getter y setters

--- src/java/modelo/modelo1.java
+++ src/java/modelo/modelo1.java
@@ -10,5 +10,60 @@ package modelo;
  * @author jeferson
  */
 public class modelo1 {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private int telefono;
+
+    public modelo1(int id, String nombre, String apellido, String correo, int telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
     
 }
